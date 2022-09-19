260a218ddb17cfbddd255fca55b571b0694dfc00
Jossy <jossyojih@gmail.com>
2022-09-19 16:14:51 +0100

    account complete without twilo

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports={
     MONGOURI:process.env.MOGOURI,
-    JWT_SECRET:process.env.JWT_SEC,
+    JWT_SECRET:process.env.JWT_SECRET,
+    SENDGRID_API:process.env.SENDGRID_API,
     EMAIL:process.env.EMAIL,
     accountSid:process.env.TWILIO_SID,
     authToken:process.env.AUTH_TOKEN,
